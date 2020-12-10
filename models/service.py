dfe01a9f32e2e7301156c0ee47d6e0769eb290ee
mohkija <petro.hypponen@edu.turkuamk.fi>
2020-12-10 13:17:25 +0200

    created service.py under models

---
+++ models/service.py
@@ -0,0 +1,21 @@
+service_list = []
+
+
+def get_last_id():
+    if service_list:
+        last_service = service_list[-1]
+    else:
+        return 1
+    return last_service.id + 1
+
+
+class Recipe:
+    def __init__(self, name, description, duration):
+        self.id = get_last_id()
+        self.name = name
+        self.description = description
+        self.duration = duration
+
+    @property
+    def data(self):
+        return {'id': self.id, 'name': self.name, 'description': self.description, 'num_of_servings': self.duration}

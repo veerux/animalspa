4847c478ea67ee7e881005cafe948de89f3a9c15
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2020-12-11 11:56:33 +0200

    changes in app.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{your_name}:{your_password}@localhost/{db_name}'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://group:12345@localhost/animalspa_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

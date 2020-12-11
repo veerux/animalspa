40df5ba16124cd6ea0758f90f0fb609952dd2c6d
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2020-12-11 11:40:21 +0200

    changes in app.py

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
+
+from config import Config
+from extensions import db
+from models.user import User
 from resources.service import ServiceListResource, ServiceResource, ServicePublishResource
 
-app = Flask(__name__)
-api = Api(app)
-api.add_resource(ServiceListResource, '/services')
-api.add_resource(ServiceResource, '/services/<int:service_id>')
-api.add_resource(ServicePublishResource, '/services/<int:service_id>/publish')
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_resources(app)
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+def register_resources(app):
+    api = Api(app)
+    api.add_resource(ServiceListResource, '/services')
+    api.add_resource(ServiceResource, '/services/<int:service_id>')
+    api.add_resource(ServicePublishResource, '/services/<int:service_id>/publish')
+
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app = create_app()
+    app.run()

999f3c486d2b2dd9576c09656cc3f77f15321faa
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2020-12-12 14:24:43 +0200

    fixing resources/reservation

--- resources/reservation.py
+++ resources/reservation.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restful import Resource
 from http import HTTPStatus
 from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_optional
-
+from models.reservation import Reservation
 
 class ReservationListResource(Resource):
     def get(self):

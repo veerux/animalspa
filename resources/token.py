1b6af842bd30359693468f1a9705d55be04ca944
mohkija <petro.hypponen@edu.turkuamk.fi>
2020-12-14 13:15:30 +0200

    loput rivikorjaukset

--- resources/token.py
+++ resources/token.py
@@ -1,14 +1,8 @@
 from http import HTTPStatus
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import (
-    create_access_token,
-    create_refresh_token,
-    jwt_refresh_token_required,
-    get_jwt_identity,
-    jwt_required,
-    get_raw_jwt
-)
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity,
+                                jwt_required, get_raw_jwt)
 from utils import check_password
 from models.user import User
 

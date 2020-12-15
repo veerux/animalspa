7ccd501d0a43a08b57c9ad5eb38a423777db4c19
suviorling <suviorling@users.noreply.github.com>
2020-12-15 20:07:55 +0200

    Schema fix

--- schemas/service.py
+++ schemas/service.py
@@ -2,19 +2,17 @@ from marshmallow import Schema, fields, post_dump, validate, validates, Validati
 from schemas.user import UserSchema
 
 
-class ServiceSchema(Schema):
+class ReservationSchema(Schema):
     class Meta:
         ordered = True
-    id = fields.Integer(dump_only=True)
-    name = fields.String(required=True, validate=[validate.Length(max=100)])
-    pet = fields.String(validate=[validate.Length(max=200)])
-    service = fields.Integer()
-    is_publish = fields.Boolean(dump_only=True)
-
-    author = fields.Nested(UserSchema, attribute='user', dump_only=True, only=['id', 'username'])
-
-    created_at = fields.DateTime(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
+        id = fields.Integer(dump_only=True)
+        name = fields.String(required=True, validate=[validate.Length(max=100)])
+        description = fields.String(validate=[validate.Length(max=500)])
+        duration = fields.Integer()
+        is_publish = fields.Boolean(dump_only=True)
+        author = fields.Nested(UserSchema, attribute='user', dump_only=True, only=['id', 'username'])
+        created_at = fields.DateTime(dump_only=True)
+        updated_at = fields.DateTime(dump_only=True)
 
     @post_dump(pass_many=True)
     def wrap(self, data, many, **kwargs):

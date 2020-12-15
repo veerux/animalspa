e1fbfd13037fad8b383e8fcbea7d273b80976808
suviorling <suviorling@users.noreply.github.com>
2020-12-15 20:10:11 +0200

    Schema fix

--- schemas/reservation.py
+++ schemas/reservation.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields, post_dump, validate, validates, Validati
 from schemas.user import UserSchema
 
 
-class ServiceSchema(Schema):
+class ReservationSchema(Schema):
     class Meta:
         ordered = True
     id = fields.Integer(dump_only=True)
--- schemas/service.py
+++ schemas/service.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields, post_dump, validate, validates, Validati
 from schemas.user import UserSchema
 
 
-class ReservationSchema(Schema):
+class ServiceSchema(Schema):
     class Meta:
         ordered = True
         id = fields.Integer(dump_only=True)

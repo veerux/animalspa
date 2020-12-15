ab11c54b39f53afc420589d5a1bc3fd2baac6588
suviorling <suviorling@users.noreply.github.com>
2020-12-15 20:57:28 +0200

    models/reservation fix

--- models/reservation.py
+++ models/reservation.py
@@ -32,8 +32,10 @@ class Reservation(db.Model):
         db.session.commit()
 
     @classmethod
-    def get_all_by_user(cls, user_id, visibility='Public'):
-        if visibility == 'public' or 'private':
-            return cls.query.filter_by(user_id=user_id).all()
+    def get_all_by_user(cls, user_id, visibility='public'):
+        if visibility == 'public':
+            return cls.query.filter_by(user_id=user_id, is_publish=True).all()
+        elif visibility == 'private':
+            return cls.query.filter_by(user_id=user_id, is_publish=False).all()
         else:
             return cls.query.filter_by(user_id=user_id).all()
